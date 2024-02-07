a5e9a304396709bc6736dbf497b77ece98ce7e70
Sophie <soph1155@stud.kea.dk>
2024-02-07 14:00:41 +0100

    ny js

--- javascript/kategori.js
+++ javascript/kategori.js
@@ -26,3 +26,21 @@ function showProduct(product) {
 }
 
 
+/*
+
+const urlParams = new URLSearchParams(window.location.search);
+const season = urlParams.get("season");
+
+fetch("https://kea-alt-del.dk/t7/api/produktliste.html?season=summer" + season)
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".tilvalg h3").textContent = product.productdisplayname;
+  document.querySelector(".tilvalg .koen").textContent = product.gender;
+  document.querySelector(".tilvalg .overdel").textContent = product.subcategory;
+  document.querySelector(".tilvalg .brand").textContent = product.brandname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
+*/
